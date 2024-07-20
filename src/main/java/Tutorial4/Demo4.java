9de72e1372721be5f784929b3e0504783f5202b2
Kalpana <kalpanasabale19@gmail.com>
2024-07-20 13:23:19 +0530

    T4

---
+++ src/main/java/Tutorial4/Demo4.java
@@ -0,0 +1,11 @@
+package Tutorial4;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TC created by Kalpana Demo4");
+
+	}
+
+}
